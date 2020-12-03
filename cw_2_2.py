aee3652235ce1f4aba73c342430a6f6431e7201d
Fedor Petrenko <bitbucketsputnik@gmail.com>
2020-12-03 21:49:03 +0700

    fixes point 4
    fixes point 4

---
+++ cw_2_2.py
@@ -0,0 +1,7 @@
+
+a = bin(8)
+b = bin(10)
+
+a_b = (a, 2)
+
+print(a_b)
--- homework_point5.py
+++ homework_point5.py
@@ -6,9 +6,9 @@ employees = int(input("Employees number - \n"))
 if income > outcome:
     net_profit = income - outcome
     print("Good! Your business is profitable! You have earned - ", net_profit,"$")
-    roe = net_profit // income
+    roe = net_profit / income
     print("Your ROE = ", roe)
-    profit_per_person = income //employees
+    profit_per_person = net_profit // employees
     print("Profit per person - ", profit_per_person, "$")
 else:
     print("Very sad! Business is unprofitable :( ")
