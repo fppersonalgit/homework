13a727bf3824e1cd93cb96c321c2ba8b92d0a81c
Fedor Petrenko <bitbucketsputnik@gmail.com>
2020-12-06 19:20:56 +0700

    changes point4 for lesson 2
    changes point4 for lesson 2

---
+++ homework_lesson2_point3.py
@@ -0,0 +1,31 @@
+# 3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить к какому времени года относится месяц (зима,
+# весна, лето, осень). Напишите решения через list и через dict.
+
+month_number = int(input("Enter month number from 1 to 12 - \n"))
+
+print(month_number)
+# print(type(month_number))
+
+if month_number > 12 or 0:
+    print("No, month number can be only from 1-12. Might be on other planet there is month number - ", month_number,
+          ", but for sure not on Earth.  Please try again...")
+
+# seasons = {
+#    "Winter": (1, 2, 12),
+#    "Spring": (3, 4, 5),
+#    "Summer": (6, 7, 8),
+#    "Autumn": (9, 10, 11)}
+
+# for key in seasons.keys():
+#    if month_number in seasons[key]:
+#        print(key)
+
+seasons_list = list[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+for x in
+    if month_number == 1 or 2 or 12:
+        print("Winter")
+
+
+
+
